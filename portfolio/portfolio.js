38104a2ecde42fd367f94d186331b8bc603fa842
Navneet Singh <navneetsingh.2806@gmail.com>
2024-09-25 19:57:26 +0530

    Portfolio

---
+++ portfolio/portfolio.js
@@ -0,0 +1,11 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablinks of tablinks){
+        tablelink.classList.remove("active-links");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+}
